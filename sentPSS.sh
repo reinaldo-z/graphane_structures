81d20845a2d9cdea5970f873b590808e65ce9dac
reychino-heisenberg <reychino@gmail.com>
2015-11-18 22:47:44 -0600

    Mario corrections and band structure figure.

--- sentPSS.sh
+++ sentPSS.sh
@@ -2,10 +2,10 @@
 
 cp w2sp-pss.clo sentPSS/
 cp wiley2sp.cls sentPSS/
-
-cp graphane_structures.tex sentPSS/graphene_structuresPSS.tex
-cp graphane_structures.bib sentPSS/graphene_structuresPSS.bib
 cp pss.bst sentPSS/pss.bst
+
+cp pssb.201552565.tex sentPSS/graphene_structuresPSS.tex
+cp graphane_structures.bib sentPSS/graphene_structuresPSS.bib
 
 cp figures/images/graph1.png  sentPSS/
 cp figures/images/graph2.png  sentPSS/
@@ -14,17 +14,17 @@ cp figures/images/alt2.png  sentPSS/
 cp figures/images/up1.png  sentPSS/
 cp figures/images/up2.png  sentPSS/
 
-cp figures/dsp-alt.eps  sentPSS/
-cp figures/dsp-up.eps  sentPSS/
-cp figures/eta-alt_x.eps  sentPSS/
+cp figures/dsp-alt.pdf  sentPSS/
+cp figures/dsp-up.pdf  sentPSS/
+cp figures/eta-alt_x.pdf  sentPSS/
 cp figures/eta-alt_y.eps  sentPSS/
 cp figures/eta-alt_z.eps  sentPSS/
-cp figures/eta-up_x.eps  sentPSS/
+cp figures/eta-up_x.pdf  sentPSS/
 cp figures/eta-up_y.eps  sentPSS/
 cp figures/eta-up_z.eps  sentPSS/
-cp figures/shg-vnl-alt.eps  sentPSS/
+cp figures/shg-vnl-alt.pdf  sentPSS/
 cp figures/shg-lay-alt.eps  sentPSS/
-cp figures/shg-vnl-up.eps  sentPSS/
+cp figures/shg-vnl-up.pdf  sentPSS/
 cp figures/shg-lay-up.eps  sentPSS/
 
 sed -i -- 's/figures\///g' sentPSS/graphene_structuresPSS.tex
